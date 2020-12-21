387c7392ecc6c7fc88edbfd3c26edbfb442be82b
Juan Trinidad <juan_trinidad14@hotmail.com>
2020-12-21 15:39:05 -0500

    login form

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,10 +1,46 @@
-
-import './App.css';
+import React, { useState } from 'react'
+import LoginForm from './Components/LoginForm'
 
 function App() {
+  const adminUser ={
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""})
+  const [error, setError]= useState("")
+
+  const Login = details => {
+    console.log(details)
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Loggen In")
+      setUser({
+        name: details.name,
+        lastName: details.lastName,
+        email: details.email
+      })
+    } else {
+      console.log("Details do not Match")
+      setError("Details do not match ")
+    }
+  }
+  const Logout = () => {
+    console.log("Logout")
+    setUser({ name:"",lastName:"", email: ""})
+   
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+     {(user.email != "") ? (
+      <div className="welcome">
+        <h2>Welcome, <span>{user.name}</span></h2>
+        <button onClick={Logout}>Logout</button>
+      </div>
+     ) : (
+       <LoginForm Login={Login} error={error}/>
+     )}
     </div>
   );
 }
