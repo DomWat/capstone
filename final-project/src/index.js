f7486269166da26b1ab9176ed5a508d6ca7f3487
Juan Trinidad <juan_trinidad14@hotmail.com>
2020-12-21 17:04:39 -0500

    login

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -14,6 +14,7 @@ import Login from './Components/Login'
 import Classes from './Components/Classes'
 
 
+
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
