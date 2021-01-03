7c43d04d00e4e1852758e8c0bbc8ac3fc44c0f96
wfoody <foodyw@gmail.com>
2021-01-03 14:52:13 -0500

    home page text and styles

--- final-project/src/Components/Home1.js
+++ final-project/src/Components/Home1.js
@@ -6,7 +6,7 @@ function Home1() {
     return (
     <div className='home1Container'>
         
-        <p className='home1text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p className='home1text'>GoodTutor is an accessible, online-learning platform designed to provide free tutoring services to students of all ages. With personalized, one-on-one lessons, GoodTutor offers the direct engagement that many students need in order to learn and succeed - at no cost.  </p>
         <img src={image} className='home1pic' />
     
     </div>)
