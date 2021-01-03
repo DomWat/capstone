0b15a5138042ce409a3226ca038d46db0023cdb3
DomWat <domwat20@gmail.com>
2021-01-03 14:28:07 -0600

    tutor profile

--- final-project/src/Components/TutorProfile.js
+++ final-project/src/Components/TutorProfile.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Table from "react-bootstrap/Table"
+import '../styles/StudentProfile.css'
 
 function TutorProfile() {
   const [tutor, setTutor] = useState({
-    student: {
+    tutor: {
       Appointments: [],
+      Schedule: {},
+      Subjects: []
     },
   });
   useEffect(() => {
@@ -23,59 +27,83 @@ function TutorProfile() {
   };
 
   return (
+    <>
       <div>
-        <h1>working?</h1>
-        <p>{tutor.tutor.first_name}</p>
+        <h1 className="users-title">
+          <b>User's Information</b>
+
+        </h1>
+        <div className="photo">
+          <label className="photo-title">
+            <b>Tutor Photo:</b>
+          </label>
+          <p className="student-photo">{tutor.tutor.image}</p>
+        </div>
+        <div className="content">
+          <label className="users-information">
+            <b>Name:</b>
+          </label>
+          <p className="information">{tutor.tutor.first_name}</p>
+          <label className="users-information">
+            <b>Last Name:</b>
+          </label>
+          <p className="information">{tutor.tutor.last_name}</p>
+          <label className="users-information">
+            <b>Email:</b>
+          </label>
+          <p className="information">{tutor.tutor.email}</p>
+          <label className="users-information">
+            <b>Description:</b>
+          </label>
+          <p className="information">{tutor.tutor.description}</p>
+        </div>
       </div>
-    // <>
-    //   <div>
-    //     <h1>
-    //       <b>User Information</b>
-    //     </h1>
-    //     <label>
-    //       <b>Name:</b>
-    //     </label>
-    //     <p>{tutor.tutor.first_name}</p>
-    //     <label>
-    //       <b>Last Name:</b>
-    //     </label>
-    //     <p>{tutor.tutor.last_name}</p>
-    //     <label>
-    //       <b>Email:</b>
-    //     </label>
-    //     <p>{tutor.tutor.email}</p>
-    //     <label>
-    //       <b>Student Photo:</b>
-    //     </label>
-    //     <p>{tutor.tutor.image}</p>
-    //   </div>
-    //   <h1>Appointment History</h1>
-    //   {student.student.Appointments.map((appointment) => {
-    //     return (
-    //       <div>
-    //         <h6>
-    //           <b>Tutor Details</b>
-    //         </h6>
-    //         <p>
-    //           Name: {appointment.Tutor.first_name} {appointment.Tutor.last_name}
-    //         </p>
-    //         <p>
-    //           Email:<i> {appointment.Tutor.email}</i>
-    //         </p>
-    //         <h6>
-    //           <b>Subject Details</b>
-    //         </h6>
-    //         <p>Subject: {appointment.Subject.subject_name}</p>
-    //         <p>Specialty: {appointment.Subject.sub_subject_name}</p>
-    //         <h6>
-    //           <b>Appointment Details</b>
-    //         </h6>
-    //         <p>Start time: {appointment.start_time}</p>
-    //         <p>End time: {appointment.end_time}</p>
-    //       </div>
-    //     );
-    //   })}
-    // </>
+      <h1>Subject: </h1>
+      {tutor.tutor.Subjects.map((subject) => {
+        return <div key = {subject.subject_id}>{subject.subject_name} Specialty: {subject.sub_subject_name}</div>
+      })}
+      <h1>Schedule: </h1>
+      Mon: {tutor.tutor.Schedule.monday}
+      Tues: {tutor.tutor.Schedule.tuesday}
+      Wed: {tutor.tutor.Schedule.wednesday}
+      Thur: {tutor.tutor.Schedule.thursday}
+      Fri: {tutor.tutor.Schedule.friday}
+      Sat: {tutor.tutor.Schedule.saturday}
+      Sun: {tutor.tutor.Schedule.sunday}
+      <h1 className='appointment-title'><b>Appointment History</b></h1>
+      {tutor.tutor.Appointments.map((appointment) => {
+        return (
+          <div className="Container-2">
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Tutor's  First Name:</th>
+                  <th>Tutor's Last Name:</th>
+                  <th>Tutor's E-Mail:</th>
+                  <th>Subject:</th>
+                  <th>Specialty:</th>
+                  <th>Date:</th>
+                  <th>Start Time:</th>
+                  <th>End Time:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{appointment.Student.first_name} </td>
+                  <td>{appointment.Student.last_name}</td>
+                  <td><i> {appointment.Student.email}</i></td>
+                  <td>{appointment.Subject.subject_name}</td>
+                  <td>{appointment.Subject.sub_subject_name}</td>
+                  <td></td>
+                  <td>{appointment.start_time}</td>
+                  <td>{appointment.end_time}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
