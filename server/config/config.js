88777885ec66a12e4b6f6f6b570f94ad2476332e
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2021-01-06 15:48:42 -0500

    .env

--- server/config/config.js
+++ server/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "ewwbgtka",
-    password: "YesDKv_BUAen3IKXTarSG7Y1wTj9zETQ",
-    database: "ewwbgtka",
-    host: "suleiman.db.elephantsql.com",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
   test: {
-    username: "ewwbgtka",
-    password: "YesDKv_BUAen3IKXTarSG7Y1wTj9zETQ",
-    database: "ewwbgtka",
-    host: "suleiman.db.elephantsql.com",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
   production: {
-    username: "ewwbgtka",
-    password: "YesDKv_BUAen3IKXTarSG7Y1wTj9zETQ",
-    database: "ewwbgtka",
-    host: "suleiman.db.elephantsql.com",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
